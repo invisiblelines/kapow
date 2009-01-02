c28875d83fbb17b6bad8b5da0fb08a015559e8f3
Kieran Johnson <kieran@invisiblelines.com>
2009-01-02 11:04:16 +0000

    clean up sms spec

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -17,7 +17,6 @@ describe Kapow::SMS do
   
     it "should send an sms" do
       Net::HTTP.should_receive(:post_form).with(@uri, {:username => "kieran", :mobile => "4401234567890", :password => "secret", :sms => "Yo!"}).and_return(@response)
-      @response.should_receive(:body)
       lambda { @sms.deliver("4401234567890", "Yo!") }.should_not raise_error
     end
     
@@ -25,7 +24,6 @@ describe Kapow::SMS do
     
       it "should send an sms as a flash" do
         Net::HTTP.should_receive(:post_form).with(@uri, {:flash => true, :username => "kieran", :mobile => "4401234567890", :password => "secret", :sms => "FLASHYo!"}).and_return(@response)
-        @response.should_receive(:body)
         lambda { @sms.deliver("4401234567890", "Yo!", :flash => true) }.should_not raise_error
       end
     
@@ -35,7 +33,6 @@ describe Kapow::SMS do
     
       it "should send an sms with a from_id" do
         Net::HTTP.should_receive(:post_form).with(@uri, {:from_id => "Kieran_j", :username => "kieran", :mobile => "4401234567890", :password => "secret", :sms => "Yo!"}).and_return(@response)
-        @response.should_receive(:body)
         lambda { @sms.deliver("4401234567890", "Yo!", :from_id => "Kieran_j") }.should_not raise_error
       end
     
@@ -47,7 +44,6 @@ describe Kapow::SMS do
         msg = ""
         4.times { msg << "The quick brown fox jumps over the lazy dog. " }
         Net::HTTP.should_receive(:post_form).with(@uri, {:long_sms => true, :username => "kieran", :mobile => "4401234567890", :password => "secret", :sms => msg}).and_return(@response)
-        @response.should_receive(:body)
         lambda { @sms.deliver("4401234567890", msg, :long_sms => true) }.should_not raise_error
       end
     
